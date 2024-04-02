26447ae5f06265f3126d35e33ed04959dd99e7b7
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-02 16:24:16 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -39,7 +39,7 @@ function love.run()
 	end
 end
 --love.filesystem = require("extern.nativefs")
-local files = love.filesystem.getDirectoryItems("")
+local files = love.filesystem.getDirectoryItems(love.filesystem.getSaveDirectory())
 table.insert(files, "There could be your ad")
 for a, b in ipairs(files) do
    print(a, b) 
