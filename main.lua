8b1c33459688f53f065f43f5ae9706397420bf3c
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-02 16:20:24 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -39,7 +39,7 @@ function love.run()
 	end
 end
 love.filesystem = require("extern.nativefs")
-local files = love.filesystem.getDirectoryItems("~/home/yellowbutshort/code")
+local files = love.filesystem.getDirectoryItems("./home/yellowbutshort/code")
 table.insert(files, "There could be your ad")
 for a, b in ipairs(files) do
    print(a, b) 
