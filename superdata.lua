6070805cff39b0c8dd35bb5e15a0e00905d7a8da
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-02 15:57:21 +0300

    Update superdata.lua

--- superdata.lua
+++ superdata.lua
@@ -1,4 +1,4 @@
-PATH_PROMOCODES = "~/realgarbo/promocodes.json"
-PATH_DB_USERS = "~/realgarbo/db/userlist.db"
-PATH_DB_CHATS = "~/realgarbo/db/chats.db"
-PATH_DB = "~/realgarbo/db/chats.db"
+PATH_PROMOCODES = "./realgarbo/promocodes.json"
+PATH_DB_USERS = "./realgarbo/db/userlist.db"
+PATH_DB_CHATS = "./realgarbo/db/chats.db"
+PATH_DB = "./realgarbo/db/chats.db"
