2c53486be49aa0e70c3a71f9c03469e1f5d9db83
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-02 15:50:20 +0300

    huh

--- main.lua
+++ main.lua
@@ -1,4 +1,45 @@
 
+function love.run()
+	if love.load then love.load(love.parsedGameArguments, love.rawGameArguments) end
+
+	-- We don't want the first frame's dt to include time taken by love.load.
+	if love.timer then love.timer.step() end
+
+	-- Main loop time.
+	return function()
+		-- Process events.
+		if love.event then
+			love.event.pump()
+			for name, a,b,c,d,e,f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() then
+						return a or 0, b
+					end
+				end
+				love.handlers[name](a,b,c,d,e,f)
+			end
+		end
+
+		-- Update dt, as we'll be passing it to update
+		local dt = love.timer and love.timer.step() or 0
+
+		-- Call update and draw
+		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+
+		--if love.graphics and love.graphics.isActive() then
+		--	love.graphics.origin()
+		--	love.graphics.clear(love.graphics.getBackgroundColor())
+
+		--	if love.draw then love.draw() end
+
+		--	love.graphics.present()
+		--end
+
+		if love.timer then love.timer.sleep(0.001) end
+	end
+end
+
+
 --[[
 telelove = require("extern.Telelove")
 
