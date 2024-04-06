03c7ecf4f0aef8482e5ea70194d13fde7e74eb4c
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-07 00:10:06 +0300

    Create webhook_handler.py

---
+++ webhook_handler.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Here you can process the data received in the POST request
+    data = request.get_json()
+    print('Received data: ', data)
+    return 'Success', 200
+if __name__ == '__main__':
+    app.run(host="158.160.134.126", debug=True)
