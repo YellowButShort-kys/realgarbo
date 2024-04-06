f6cb8562637a3ada7aa4d9ad23fb44c2232a4fb6
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-06 18:33:13 +0300

    Update horde.lua

--- api/horde.lua
+++ api/horde.lua
@@ -131,6 +131,7 @@ local headers = {
 }
 
 function horde.Generate(prompt, callback, errcallback, extra, stop_sequence)
+    print(prompt)
     return __promise(0.5, horde.__Generate, prompt, callback, errcallback, extra, stop_sequence)
 end
 function horde.__Generate(prompt, callback, errcallback, extra, stop_sequence)
