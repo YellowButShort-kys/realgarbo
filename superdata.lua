b4202be125848dd7d005281d9bdd203e3e7ff2ca
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-07 00:11:00 +0300

    Update superdata.lua

--- superdata.lua
+++ superdata.lua
@@ -1,6 +1,6 @@
 PATH_PROMOCODES = "promocodes.json"
 PATH_DB_USERS = love.filesystem.getRealDirectory("db/userlist.db") .. "db/userlist.db"
-PATH_DB_CHATS = love.filesystem.getRealDirectory("db/userlist.db") .. "db/chats.db"
+PATH_DB_CHATS = love.filesystem.getRealDirectory("db/chats.db") .. "db/chats.db"
 print("", PATH_DB_USERS)
 print("", PATH_DB_CHATS)
 
