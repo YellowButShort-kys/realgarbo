af2bb463374dbeaab92741e78fc3e8f70135528c
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-06 17:44:35 +0300

    Update init.lua

--- db/init.lua
+++ db/init.lua
@@ -68,7 +68,13 @@ local query_check_if_exists = [[
         content TEXT
     );
 ]]
-local query_add_chat =  [[ 
+local query_add_chat =  [[
+    CREATE TABLE IF NOT EXISTS "%s" (
+        id INTEGER PRIMARY KEY,
+        content TEXT,
+        role TEXT
+    );
+     
     INSERT INTO "%s" (
         id,
         role,
@@ -393,14 +399,14 @@ function db_Load()
             local commit = sqlite3.open(PATH_DB_CHATS)
             commit:execute(query_check_if_exists:format(chat.owner.id))
             do 
-                local stmt = commit:prepare(query_add_chat:format(chat.id .. "_" .. chat.owner.id))
+                local stmt = commit:prepare(query_add_chat:format(chat.id .. "_" .. chat.owner.id, chat.id .. "_" .. chat.owner.id))
                 stmt:bind_values(chat.id, "system", chat.char:GetSystem(chat.owner))
                 stmt:step()
                 stmt:finalize()
                 table.insert(chat.content, {id = 1, role = "system", content = chat.char:GetSystem(chat.owner)})
             end
             do 
-                local stmt = commit:prepare(query_add_chat:format(chat.id .. "_" .. chat.owner.id))
+                local stmt = commit:prepare(query_add_chat:format(chat.id .. "_" .. chat.owner.id, chat.id .. "_" .. chat.owner.id))
                 stmt:bind_values(chat.id, "assistant", chat.char:GetGreeting(chat.owner))
                 stmt:step()
                 stmt:finalize()
