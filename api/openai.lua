e19b47f3beefb85569e5b9c9964584836708009d
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-19 19:09:08 +0300

    Update openai.lua

--- api/openai.lua
+++ api/openai.lua
@@ -23,8 +23,7 @@ local data = {
 }
 local megacallback = function(success, errcode, result, extra)
     if success then
-        print(telelove.json.encode(result))
-        extra.kudos = result.total_tokens / 10
+        extra.kudos = result.usage.total_tokens / 10
         extra:callback(result.choices[1].message.content or " ")
     else
         print(errcode)
