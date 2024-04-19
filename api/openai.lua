15b86e575127e4b02a3257f37a73668dca570990
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-19 22:42:13 +0300

    Update openai.lua

--- api/openai.lua
+++ api/openai.lua
@@ -23,7 +23,7 @@ local data = {
 }
 local megacallback = function(success, errcode, result, extra)
     if success then
-        extra.kudos = result.usage.total_tokens / 50
+        extra.kudos = math.ceil(result.usage.total_tokens / 50)
         extra:callback(result.choices[1].message.content or " ")
     else
         print(errcode)
