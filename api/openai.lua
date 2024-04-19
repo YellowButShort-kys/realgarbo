99ba71bc0e1567ec1d66621be7613b556a0153ee
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-04-19 18:11:16 +0300

    Update openai.lua

--- api/openai.lua
+++ api/openai.lua
@@ -3,15 +3,22 @@ local LINK = "https://unicorn.dragonetwork.pl/proxy/openai/chat/completions"
 requests.Init(6, 0.05, 24)
 
 local data = {
-    ["model"] = "gpt-3.5-turbo",
-    ["max_tokens"] = 250,
-    ["temperature"] = 0.9,
-    ["top_p"] = 1,
-    ["presence_penalty"] = 0,
-    ["frequency_penalty"] = 0,
-    ["stop"] = {"<|endoftext|>"},
-    ["messages"] = {
-        
+    headers = {
+        ["Content-Type"] = "application/json",
+        ["Authorization"] = "Bearer unicorn"
+    },
+    method = "POST",
+    data = {
+        ["model"] = "gpt-3.5-turbo",
+        ["max_tokens"] = 250,
+        ["temperature"] = 0.9,
+        ["top_p"] = 1,
+        ["presence_penalty"] = 0,
+        ["frequency_penalty"] = 0,
+        ["stop"] = {"<|endoftext|>"},
+        ["messages"] = {
+            
+        }
     }
 }
 local megacallback = function(success, errcode, result, extra)
@@ -30,15 +37,15 @@ end
 
 function openai.Generate(messages, callback, errcallback, extra, stop_sequence)
     print("Generate")
-    local old_messages = data["messages"]
-    local old_stop = data["stop"]
-    data["messages"] = messages
-    data["stop"] = {}
+    local old_messages = data["data"]["messages"]
+    local old_stop = data["data"]["stop"]
+    data["data"]["messages"] = messages
+    data["data"]["stop"] = {}
     for _, var in ipairs(old_stop) do
-        table.insert(data["stop"], var)
+        table.insert(data["data"]["stop"], var)
     end
     for _, var in ipairs(stop_sequence) do
-        table.insert(data["stop"], var)
+        table.insert(data["data"]["stop"], var)
     end
     
     local task = {}
@@ -49,8 +56,8 @@ function openai.Generate(messages, callback, errcallback, extra, stop_sequence)
     print("Request submitted")
     requests.Request(LINK, data, megacallback, task)
     print("Success")
-    data["messages"] = old_messages
-    data["stop"] = old_stop
+    data["data"]["messages"] = old_messages
+    data["data"]["stop"] = old_stop
     
     return task
 end
