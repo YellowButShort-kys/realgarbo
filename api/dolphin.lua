bf44935db4ad7e71b464a318c6a0768438d7a5c1
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-05-07 16:21:42 +0300

    Update dolphin.lua

--- api/dolphin.lua
+++ api/dolphin.lua
@@ -16,7 +16,7 @@ local data = {
         --["top_p"] = 1,
         --["presence_penalty"] = 0,
         --["frequency_penalty"] = 0,
-        ["stop"] = {"<|endoftext|>", "<START>"},
+        ["stop"] = {"<|endoftext|>", "<START>", "<|eot_id|>"},
         ["messages"] = {
             
         }
