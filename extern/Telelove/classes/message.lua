7df3fffe2b33a9e6cfd2432bc2e0e7c0dee6b0fd
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-05-23 12:49:20 +0300

    Update message.lua

--- extern/Telelove/classes/message.lua
+++ extern/Telelove/classes/message.lua
@@ -27,9 +27,11 @@ return function(classes)
                     {method = "POST", headers = {["Content-Type"] = "application/json"}, data = client.__telelove.json.encode({chat_id = self.chat.id, message_id = self.message_id})}
                 )
                 
-                if code ~= 200 then
-                    client.__telelove.__error("Failed while deleting a message! Error code: "..code)
+                if code == 0 then
                     return false
+                elseif code ~= 200 then
+                    client.__telelove.__error("Failed while deleting a message! Error code: "..code)
+                    return true
                 else
                     return true
                 end
