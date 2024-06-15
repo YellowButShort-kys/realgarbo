8b4c5f466183c97c3ce1392e222b1b2cad987b97
YellowButShort <59924622+YellowButShort-kys@users.noreply.github.com>
2024-06-16 00:38:22 +0300

    f

--- commands/profile.lua
+++ commands/profile.lua
@@ -23,7 +23,7 @@ return function(langcode, menu, button)
             local subtokens = user.subscriptiontokens
             local refcode = encode(user.id)
             local reflink = "https://t.me/CarpAI_bot?start="..refcode
-        client:EditMessageText(query.message.chat, query.message, LANG[langcode]["$PROFILE_TEXT"]:format(tostring(display_name), tostring(balance), tostring(sublevel), tostring(subtokens), tostring(refcode), tostring(reflink)), profile_ikm)
+        client:EditMessageText(query.message.chat, query.message, LANG[langcode]["$PROFILE_TEXT"]:format(tostring(display_name), tostring(balance), tostring(sublevel), tostring(subtokens), "%", tostring(refcode), tostring(reflink)), profile_ikm)
     end
     
     local display_name = client:NewInlineKeyboardButton()
