f300362b977d259a510fe799dd2fee54a6838aff
YellowButShort-kys <59924622+YellowButShort-kys@users.noreply.github.com>
2024-09-28 01:58:20 +0300

    Update sciencev2.lua

--- sciencev2.lua
+++ sciencev2.lua
@@ -3,13 +3,13 @@ local pool = requests.CreatePool(2)
 sciencev2 = {}
 
 function sciencev2.onNewUser()
-    pool:Request("https://localhost:5000/AppendNewUsers")
+    pool:Request("https://84.201.144.160:5000/AppendNewUsers")
 end
 
 function sciencev2.onTokensSpent(amount)
-    pool:Request("https://localhost:5000/AppendTokensSpent", {method = "post", headers = {["Content-Type"] = "application/json"}, data = {TokensSpent = amount}})
+    pool:Request("https://84.201.144.160:5000/AppendTokensSpent", {method = "post", headers = {["Content-Type"] = "application/json"}, data = {TokensSpent = amount}})
 end
 
 function sciencev2.onNewChat()
-    pool:Request("https://localhost:5000/AppendNewChats")
+    pool:Request("https://84.201.144.160:5000/AppendNewChats")
 end
